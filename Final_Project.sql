f76b1b24827bd5344f93c1128b400fe294d2e655
Maddy! <127909279+madmargo@users.noreply.github.com>
2026-01-08 20:06:05 -0500

    Split data into classifications
    Classified data into periods: paying and non-paying users for both Q2 in 2021 and 2022.

---
+++ Final_Project.sql
@@ -0,0 +1,60 @@
+drop view if exists purchases_info;
+
+create view purchases_info as
+select
+	*,
+    case
+		when date_start <= '2021-06-30' and (date_end >= '2021-04-01' or date_end is null)
+        then 1 
+        else 0
+	end as paid_q2_2021,
+    case
+		when date_start <= '2022-06-30' and (date_end >= '2022-04-01' or date_end is null)
+        then 1
+        else 0
+	end as paid_q2_2022
+from
+(select
+	purchase_id,
+    student_id,
+    plan_id,
+    date_start,
+    if(date_refunded is null,
+		date_end,
+        date_refunded) as date_end
+from
+(select
+	purchase_id,
+    student_id,
+    plan_id,
+    date_purchased as date_start,
+    case
+		when plan_id = 0 then date_add(date_purchased, interval 1 month)
+        when plan_id = 1 then date_add(date_purchased, interval 3 month)
+        when plan_id = 2 then date_add(date_purchased, interval 12 month)
+        when plan_id = 3 then null
+	end as date_end,
+    date_refunded
+from
+	student_purchases) a) b;
+
+select
+	a.student_id,
+    a.minutes_watched,
+    if(max(i.paid_q2_2022) = 1, 1, 0) as paid_in_q2
+from
+(select
+	student_id,
+    round(sum(seconds_watched) / 60, 2) as minutes_watched
+from
+	student_video_watched
+where
+	date_watched between '2022-04-01' and '2022-06-30'
+group by
+	student_id) a
+left join 
+	purchases_info i on a.student_id = i.student_id
+group by
+	a.student_id
+having
+	paid_in_q2 = 1;
